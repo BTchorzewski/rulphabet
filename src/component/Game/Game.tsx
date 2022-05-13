01dd559f8638fd652cdcbf5ab75084bcb5eceecb
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-05-13 15:41:36 +0200

    split content to components

--- src/component/Game/Game.tsx
+++ src/component/Game/Game.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import './Game.scss';
-import { alphabet } from '../../utilities/alphabet';
-import { Sign } from './Sign';
+import { Sentence } from './Sentence';
 
 interface Props {
   song: string
 }
 
 export const Game = (props: Props) => {
+  const sentenceReg = /[.]/gi;
+  const sentences = props.song.split(sentenceReg);
   return (
     <div className="game">
       {
-        props.song.split('').map(el => {
-          const foundSign = alphabet.filter(sign => sign.sign === el);
-          if (foundSign.length < 1) return <Sign sign={el} omit={true} spelling={el} pronunciation={el}/>;
-          return <Sign sign={el} omit={false} pronunciation={foundSign[0].pronunciation}
-                       spelling={foundSign[0].spelling}/>
-        })
+        sentences.map(sentence => <Sentence sentence={sentence}></Sentence>)
       }
     </div>
   )
